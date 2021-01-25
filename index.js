a4fd73161e009693b2dd50b805c80a69c6b2e020
NickSpangler <Spangleyman@mac.com>
2021-01-25 14:51:24 -0500

    attempt DFS()

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function depthFirstSearch(rootNode, vertices, edges){
+    let stack = [rootNode]
+    let dfsOrder = []
+    while (stack.length !== 0) {
+        let currentNode = stack.pop();
+        if (!dfsOrder.includes(currentNode)) {
+            let adjacentNodes = findAdjacentNodes(currentNode, vertices, edges);
+            stack.concat(adjacentNodes);
+            dfsOrder.push(currentNode);
+        }
+    }
+    return dfsOrder;
+}
 
+function findAdjacentNodes(node, vertices, edges) {
+    let 
 }
