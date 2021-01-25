3b884d4c6f3164a5c2e46bc3552c183bf3fa9aee
NickSpangler <Spangleyman@mac.com>
2021-01-25 15:31:45 -0500

    deal with typo

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function depthFirstSearch(rootNode, vertices, edges){
-    let stack = [rootNode]
-    let dfsOrder = []
+    let stack = []
+    stack.push(rootNode)
+    let dfsOrder = [rootNode]
     while (stack.length !== 0) {
         let currentNode = stack.pop();
-        if (!dfsOrder.includes(currentNode)) {
+        // console.log(currentNode.name)
+        if (currentNode.discovered !== true) {
+            currentNode.discovered = true;
+            // console.log('not in dfsOrder yet')
             let adjacentNodes = findAdjacentNodes(currentNode, vertices, edges);
-            stack.concat(adjacentNodes);
-            dfsOrder.push(currentNode);
+            // console.log(adjacentNodes)
+            adjacentNodes.forEach(node => {
+                // node.discovered = true;
+                dfsOrder.push(node)
+                stack.push(node)
+            })
         }
     }
     return dfsOrder;
 }
 
 function findAdjacentNodes(node, vertices, edges) {
-    let 
+    let nodeName = node.name;
+    let nodeEdges = edges.filter(edge => edge.includes(nodeName));
+    let adjacentNodeNames = nodeEdges.map(edge => edge.filter(el => el !== nodeName)[0]);
+    let adjacentNodes = adjacentNodeNames.map(name => vertices.filter(vertex => vertex.name === name)[0])
+    let nonVisited = adjacentNodes.filter(node => node.discovered !== true)
+    return nonVisited;
 }
