c2375d3bb28378efc0a7734607a10aa60a516475
Jason J Cruz <jaycru06@yahoo.com>
2021-06-16 03:43:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// PSUEDOCODE Wiki:
-  // procedure DFS_iterative(G, v) is
-  //   let S be a stack
-  //   S.push(v)
-  //   while S is not empty do
-  //       v = S.pop()
-  //       if v is not labeled as discovered then
-  //           label v as discovered
-  //           for all edges from v to w in G.adjacentEdges(v) do 
-  //               S.push(w)
-
 // DFS Steps:
   // set an empty stack to store visited vertices
   // first push the root vertex onto the stack
@@ -21,19 +10,16 @@
       
       
 function depthFirstSearch(rootNode, vertices, edges) {
-  const stack = [];
   stack.push(rootNode);
-  const explored = [rootNode];
-  
-  while (stack.length !== 0) {
-    let currentVertex = stack.pop(); 
-    
-    if (currentVertex.discovered === null) {
-      currentVertex.discovered = true; // label as explored
-      
-      for (const vertex of findAdjacent(currentVertex.name, vertices, edges)) { // ['23rd&6th', '28th&Bwy']
-        explored.push(vertex);
-        stack.push(vertex); 
+  let explored = [rootNode];
+
+  while (stack.length > 0) {
+    let vertex = stack.pop();
+    if (!vertex.discovered) {
+      vertex.discovered = true;
+      for (const edge of findAdjacent(vertex.name, vertices, edges)) {
+        stack.push(edge);
+        explored.push(edge);
       }
     }
   }
@@ -59,8 +45,16 @@ function findNode(nodeName, vertices){
     return vertex.name == nodeName
   })
 }
-
+  
+function adjacentEdges(vertex, edges) {
+  console.log('vertex ', vertex);
+  const firstAdjacentIdx = edges.indexOf(edges[vertex.name]) - 1;
+  const nextAdjacentIdx = edges.indexOf(edges[vertex.name]) + 2;
+  
+  return [edges[firstAdjacentIdx], edges[nextAdjacentIdx]];
+}      
       
+
       
       
       
